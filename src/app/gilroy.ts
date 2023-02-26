795c803f3f1a48eac576b331d184a0d0d54e4c7f
hoangdangkhanh <hoangdangkhanh12c1@gmail.com>
2023-02-26 08:59:48 +0700

    first setup

---
+++ src/app/gilroy.ts
@@ -0,0 +1,56 @@
+import localFont from 'next/font/local'
+
+export const gilroy = localFont({
+  src: [
+    {
+      path: "./fonts/SVN-Gilroy Regular.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./fonts/SVN-Gilroy Italic.otf",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "./fonts/SVN-Gilroy Medium.otf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./fonts/SVN-Gilroy Medium Italic.otf",
+      weight: "500",
+      style: "italic",
+    },
+    {
+      path: "./fonts/SVN-Gilroy SemiBold.otf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "./fonts/SVN-Gilroy SemiBold Italic.otf",
+      weight: "600",
+      style: "italic",
+    },
+    {
+      path: "./fonts/SVN-Gilroy Bold.otf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./fonts/SVN-Gilroy Bold Italic.otf",
+      weight: "700",
+      style: "italic",
+    },
+    {
+      path: "./fonts/SVN-Gilroy XBold.otf",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "./fonts/SVN-Gilroy XBold Italic.otf",
+      weight: "800",
+      style: "italic",
+    },
+  ],
+});
